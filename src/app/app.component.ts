be0719cb4b9e64dc069e67f4923cd2fafc7642ed
RiKiMata3010 <phoenixarmand.ani@georgebrown.ca>
2023-04-04 00:20:39 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '101155912-lab-test02-comp3133';
+}
